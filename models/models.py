97d51207db8834bf781f321b4ea37812b92ef1ee
Wei Zhang <zhangwei1979@outlook.com>
2022-12-21 17:02:58 +0800

    order and name_get

--- models/models.py
+++ models/models.py
@@ -22,9 +22,18 @@ from odoo import models, fields
 
 class LibraryBook(models.Model):
     _name = 'library.book'
+    _description = 'Library Book'
+    _order = 'date_release desc, name'
+    _rec_name = 'short_name' #this is over write by name_get
     name = fields.Char('Title', required=True)
+    short_name = fields.Char('Short Title', required=True)
     date_release = fields.Date('Release Date')
-    author_ids = fields.Many2many(
-        'res.partner',
-        string='Authors'
-    )
+    author_ids = fields.Many2many('res.partner', string='Authors')
+
+    #need to restart odoo to pick up name_get
+    def name_get(self):
+        result = []
+        for record in self:
+            rec_name = "%s (%s)" % (record.name, record.date_release)
+            result.append((record.id, rec_name))
+        return result
