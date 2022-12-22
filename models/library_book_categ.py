581003427f1d01c00ea2e73b0864970b67bae286
Wei Zhang <zhangwei1979@outlook.com>
2022-12-22 15:34:22 +0800

    抽象模型

--- models/library_book_categ.py
+++ models/library_book_categ.py
@@ -1,8 +1,17 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
+class BaseArchive(models.AbstractModel):
+    _name = 'base.archive'
+    active = fields.Boolean(default=True)
+
+    def do_archive(self):
+        for record in self:
+            record.active = not record.active
+
 class BookCategory(models.Model):
     _name = 'library.book.category'
+    _inherit = ['base.archive']
     name = fields.Char('Category')
     parent_id = fields.Many2one('library.book.category', string='Parent Category', ondelete='restrict', index=True)
 
