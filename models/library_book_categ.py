8ea7abe287e68dbe4230d4182abc97c869624db1
Wei Zhang <zhangwei1979@outlook.com>
2022-12-22 10:11:05 +0800

    Show child categories

--- models/library_book_categ.py
+++ models/library_book_categ.py
@@ -5,6 +5,8 @@ class BookCategory(models.Model):
     _name = 'library.book.category'
     name = fields.Char('Category')
     parent_id = fields.Many2one('library.book.category', string='Parent Category', ondelete='restrict', index=True)
+
+    #By convention, One2many fields have the _ids suffix.
     child_ids = fields.One2many('library.book.category', 'parent_id', string='Child Categories')
 
     _parent_store = True
