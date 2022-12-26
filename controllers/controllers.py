2eea08df26df8e44670d5435dd557fad9843ed2b
Wei Zhang <zhangwei1979@outlook.com>
2022-12-26 21:17:42 +0800

    Controller

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,3 +1,22 @@
+from odoo import http
+from odoo.http import request
+
+class Main(http.Controller):
+    @http.route(['/my_library/books'], type='http', auth='public')
+    def books(self):
+        books = request.env['library.book'].sudo().search([])
+        html_result = '<html><body><ul>'
+        for book in books:
+            html_result += "<li> %s </li>" % book.name
+        html_result += '</ul></body></html>'
+        return html_result
+
+
+    @http.route('/my_library/books/json', type='json', auth='public')
+    def books_json(self):
+        records = request.env['library.book'].sudo().search([])
+        return records.read(['name'])
+
 # -*- coding: utf-8 -*-
 # from odoo import http
 
